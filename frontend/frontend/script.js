84606157db501c25521a3e9c9d9686b2aa590283
SUHANA S <suhanashamsak@gmail.com>
2026-01-30 12:34:34 +0530

    Delete duplicate index.html

--- frontend/frontend/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("btn").addEventListener("click", async () => {
-  const res = await fetch("http://localhost:5000/api/test");
-  const data = await res.json();
-  alert(data.message);
-});
