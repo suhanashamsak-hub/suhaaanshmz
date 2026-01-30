d3bd46b5e881337feb115f2b65427bbde0bb7630
SUHANA S <suhanashamsak@gmail.com>
2026-01-30 12:23:01 +0530

    Update script.js

--- frontend/frontend/script.js
+++ frontend/frontend/script.js
@@ -1,3 +1,5 @@
-document.getElementById("btn").addEventListener("click", () => {
-  alert("Frontend is working!");
+document.getElementById("btn").addEventListener("click", async () => {
+  const res = await fetch("http://localhost:5000/api/test");
+  const data = await res.json();
+  alert(data.message);
 });
